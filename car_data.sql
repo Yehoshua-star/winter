25a3b8061f906dc4bc865e4139585a6227617b73
YehoshuaHolstein <holsteinyehoshua@gmail.com>
2023-05-29 11:39:27 -0400

    Added our first SQL file

---
+++ car_data.sql
@@ -0,0 +1,11 @@
+USE [DatabaseStar_project]
+GO
+
+
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('Mitsubishi' ,'Eclipse')
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('Mitsubishi',  'Outlander')
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('Hyundai', 'i30')
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('Ram',   '1500')
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('Ford'   ,'F150')
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('BMW' ,'335i')
+INSERT INTO [dbo].[Cars]   ([make] ,[model]) VALUES ('BMW', '330i')
